db94ee6ce1c89e1453f2fe665f61f71dc706b48b
OwerHub <owerwild@gmail.com>
2022-02-24 14:03:25 +0100

    errors in array

---
+++ js-numerals/numberToWord.mjs
@@ -0,0 +1,137 @@
+const ones = [
+  "",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+];
+
+const tens = [
+  "",
+  "",
+  "twenty",
+  "thirty",
+  "forty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety",
+];
+
+const multiplyWords = [
+  "",
+  "hundred",
+  "thousand",
+  "million",
+  "billion",
+  "trillion",
+  "quadrillion",
+  //bigint from here
+  "quintillion",
+];
+
+const multiply = [1, 100];
+const division = [100, 10, 100];
+
+const toHundred = (numberToHundred) => {
+  return numberToHundred < 20
+    ? ones[numberToHundred]
+    : tens[Math.floor(numberToHundred / 10)] + "-" + ones[numberToHundred % 10];
+};
+
+const numberToText = (number, englishTrans = true) => {
+  const isNegative = number < 0;
+  let textArray = [];
+  number = Math.abs(number);
+
+  const errorArray = [
+    [number === 0, ["zero", false]],
+    [!number, ["Gimme' Numbers!", true]],
+    [number >= Number.MAX_SAFE_INTEGER, ["This number is out of workspace", true]],
+  ];
+
+  /* 	errorArray.forEach((error) => {
+    if (error[0]) {
+			console.log(error[1])
+      return error[1];
+    }
+  }); */
+
+  for (let i = 0; i < errorArray.length; i++) {
+    if (errorArray[i][0]) {
+      console.log(errorArray[i][1]);
+      return errorArray[i][1];
+    }
+  }
+
+  /* if (!number) {
+    return ["Gimme' Numbers!", true];
+  }
+
+  if (number >= Number.MAX_SAFE_INTEGER) {
+    return ["This number is out of workspace", true];
+  }
+
+  if (number === 0) {
+    return ["zero", false];
+  } */
+
+  if (number === 1) {
+    textArray = ["one", ""];
+  }
+
+  let i = 0;
+
+  while (
+    number > (multiply[i] || 10 ** ((i - 1) * 3)) ||
+    i === multiplyWords.length
+  ) {
+    const thousandsToHundreds =
+      englishTrans && number > 1000 && number < 2000 && i === 1;
+
+    let computedDivision = thousandsToHundreds ? 100 : division[i] || 1000;
+
+    const partNumber =
+      Math.floor(number / (multiply[i] || 10 ** ((i - 1) * 3))) % computedDivision;
+
+    //console.log(i, partNumber, thousandsToHundreds);
+
+    if (i === 1) {
+      textArray.unshift("and");
+    }
+
+    textArray.unshift(
+      partNumber < 100
+        ? toHundred(partNumber)
+        : toHundred(Math.floor(partNumber / 100)) +
+            " hundred and " +
+            toHundred(partNumber % 100),
+      multiplyWords[i]
+    );
+
+    i++;
+    thousandsToHundreds && i++;
+  }
+
+  isNegative && textArray.unshift("negative");
+
+  return [textArray.join(" ").trim(), false];
+};
+
+export { numberToText };
