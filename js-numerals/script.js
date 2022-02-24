413c5469bc99668f55b001bcc2d745b24576840c
OwerHub <owerwild@gmail.com>
2022-02-24 03:14:21 +0100

    while is working

--- js-numerals/script.js
+++ js-numerals/script.js
@@ -1,6 +1,4 @@
 const loadFunct = () => {
-  console.log("im loaded");
-
   const ones = [
     "",
     "one",
@@ -37,18 +35,42 @@ const loadFunct = () => {
     "ninety",
   ];
 
-  const toTwenty = (numberToTwenty) => {
-    return ones[numberToTwenty];
-  };
+  const multiplyWords = ["", "hundred", "tousand", "million"];
+  const multiply = [1, 100, 1000, 1000000, 10000000];
+  const division = [100, 10, 1000, 1000];
 
-  const toHundred = (numberToHundred) => {
-    if (numberToHundred < 20) {
-      return ones[numberToHundred];
+  const numberToText = (number) => {
+    const toHundred = (numberToHundred) => {
+      if (numberToHundred < 20) {
+        return ones[numberToHundred];
+      }
+      return (
+        tens[Math.floor(numberToHundred / 10)] + " " + ones[numberToHundred % 10]
+      );
+    };
+
+    let i = 0;
+    let text = "";
+
+    while (number > multiply[i]) {
+      console.log("-----");
+      const partNumber = Math.floor(number / multiply[i]) % division[i];
+
+      if (partNumber < 100) {
+        console.log(toHundred(partNumber));
+      } else {
+        console.log(
+          toHundred(Math.floor(partNumber / 100)) +
+            "hundred " +
+            toHundred(partNumber % 100)
+        );
+      }
+      console.log(multiplyWords[i]);
+      i++;
     }
-    return tens[Math.floor(numberToHundred / 10)] + "-" + ones[numberToHundred % 10];
   };
 
-  console.log(toHundred(99));
+  numberToText(1112345);
 };
 
 window.addEventListener("load", loadFunct);
